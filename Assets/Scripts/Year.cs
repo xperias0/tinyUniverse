cdbfd255ab12aa9a9ecac30afcf0db698e8a3885
xperias0 <91540085+xperias0@users.noreply.github.com>
2023-10-26 13:56:59 -0400

    Change year

--- Assets/Scripts/Year.cs
+++ Assets/Scripts/Year.cs
@@ -1,15 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Michsky.MUIP;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Year : MonoBehaviour
 {
     // Start is called before the first frame update
     public static int currentYear;
-
+    public GameObject yearSlider;
+    private Slider _sliderManager;
+    public GameObject yearText;
     private void Start()
     {
         currentYear = 2023;
+        _sliderManager = yearSlider.GetComponent<Slider>();
+    }
+
+
+
+
+    public void setYear()
+    {
+        float yearT = _sliderManager.value;
+        currentYear = (int)yearT;
+        print("Cur: " + currentYear);
+    }
+
+    public void confirmYear()
+    {
+        yearText.GetComponent<TMP_Text>().SetText(currentYear.ToString());
+        yearText.GetComponent<Animator>().SetTrigger("yearOut");
     }
 }
